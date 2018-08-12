591c1e2c69fc1766ed9f9505a488eb536435a598
Arjun <arjunsah@sussol.net>
2018-08-12 16:51:55 +0545

    Updated code with eslint setting
    Using eslint to lint and manage code.
    Updated npm start script

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 import Login from './Login';
 
-const App = () =>{
-    return(
-        <Login/>
-    )
-};
+const App = () => <Login />;
 
 export default App;
--- src/js/components/Login.js
+++ src/js/components/Login.js
@@ -1,65 +1,64 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = {
-    errorStyle: { color: 'rgb(242, 101, 50)' },
-    field: { marginBottom: '12px' },
-    fieldLast: { marginBottom: '24px' },
-    fieldFocus: { borderColor: 'rgb(242, 101, 50)' },
-    loginButtonBg: 'rgb(242, 101, 50)',
-    loginButtonLabel: '#fafafa',
-    loginWrap: {
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      position: 'absolute',
-      left: 0,
-      right: 0,
-      bottom: 0,
-      top: 0,
-    },
-    loginWrapInner: {
-      display: 'flex',
-      flexDirection: 'column',
-    },
-    noStyle: {},
-    title: { fontSize: '1.5em', color: 'rgb(242, 101, 50)', textAlign: 'center' },
-  };
+  errorStyle: { color: 'rgb(242, 101, 50)' },
+  field: { marginBottom: '12px' },
+  fieldLast: { marginBottom: '24px' },
+  fieldFocus: { borderColor: 'rgb(242, 101, 50)' },
+  loginButtonBg: 'rgb(242, 101, 50)',
+  loginButtonLabel: '#fafafa',
+  loginWrap: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    top: 0,
+  },
+  loginWrapInner: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  noStyle: {},
+  title: { fontSize: '1.5em', color: 'rgb(242, 101, 50)', textAlign: 'center' },
+};
 
-class Login extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            user: 'Arjun Sah'
-        };
-    }
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: 'Arjun',
+    };
+  }
 
-    render(){
-        const { classes } = this.props;
+  render() {
+    const { classes } = this.props;
+    const newValue = this.state;
+    return (
+      <div className={classes.loginWrap}>
+        <div className={classes.loginWrapInner}>
+          <TextField
+            id="user"
+            label="Username"
+            value={newValue.user}
+            className={classes.field}
+          />
+          <TextField
+            id="password"
+            label="Password"
+            type="password"
+            className={classes.fieldLast}
+          />
+        </div>
 
-        return(
-            <div className={classes.loginWrap}> 
-            <div className={classes.loginWrapInner}>
-                <TextField
-                    id="user"
-                    label="Username"   
-                    value={this.state.user}  
-                    className={classes.field}                        
-                    />
-                    <TextField
-                    id="password"
-                    label="Password" 
-                    type="password" 
-                    className={classes.fieldLast}
-                    />
-            </div>                                           
-                
-            </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 
 export default withStyles(styles)(Login);
